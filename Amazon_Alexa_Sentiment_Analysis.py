66c00b859cc612ff4d5acda0e117c14bd99a84df
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-13 19:34:52 +0100

    Update Amazon_Alexa_Sentiment_Analysis.py

--- Amazon_Alexa_Sentiment_Analysis.py
+++ Amazon_Alexa_Sentiment_Analysis.py
@@ -11,3 +11,17 @@ print(data.head())
 print(data.describe())
 print(data.isnull().sum())
 print(data.columns)
+
+ratings = data["rating"].value_counts()
+numbers = ratings.index
+quantity = ratings.values
+
+custom_colors = ["skyblue", "yellowgreen", 'tomato', "blue", "red"]
+plt.figure(figsize=(5, 5))
+plt.pie(quantity, labels=numbers, colors=custom_colors)
+central_circle = plt.Circle((0, 0), 0.5, color='white')
+fig = plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title("Amazon Alexa Reviews", fontsize=20)
+plt.show()
