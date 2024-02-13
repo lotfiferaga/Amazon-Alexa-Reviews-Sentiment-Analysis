af8142027fe4d314ab2bd6ad4666483667b2ae2b
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-13 19:34:28 +0100

    Update Amazon_Alexa_Sentiment_Analysis.py

--- Amazon_Alexa_Sentiment_Analysis.py
+++ Amazon_Alexa_Sentiment_Analysis.py
@@ -5,4 +5,9 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sentiments = SentimentIntensityAnalyzer()
 
 data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/amazon_alexa.tsv", delimiter='\t')
-print(data.head())                      
+print(data.head())    
+
+
+print(data.describe())
+print(data.isnull().sum())
+print(data.columns)
