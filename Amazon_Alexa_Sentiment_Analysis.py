d4e13c77a337322a819c46a8c0601c8a1dea13aa
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-13 19:34:09 +0100

    Update Amazon_Alexa_Sentiment_Analysis.py

--- Amazon_Alexa_Sentiment_Analysis.py
+++ Amazon_Alexa_Sentiment_Analysis.py
@@ -1,1 +1,8 @@
-                      
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sentiments = SentimentIntensityAnalyzer()
+
+data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/amazon_alexa.tsv", delimiter='\t')
+print(data.head())                      
